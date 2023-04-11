46188e1c5ec37f364390e594948e62476f780c7b
nicokangdra <99784038+nicokangdra@users.noreply.github.com>
2023-04-11 10:35:05 +0700

    Add files via upload

---
+++ Final SSIP MySQL/init.sql
@@ -0,0 +1,17 @@
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(100),
+    file VARCHAR(100)
+);
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(30) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
